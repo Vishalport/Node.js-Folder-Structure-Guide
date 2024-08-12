0bc4caf6cd3bd136c1479bcf5b6e6bef73065f76
Vishal Kumar <vishal.kumar@moondive.co>
2024-08-12 14:11:48 +0530

    uses: SonarSource/sonarcloud-github-action@main

--- server/api/v1/controllers/user/controller.js
+++ server/api/v1/controllers/user/controller.js
@@ -1,26 +1,37 @@
 
 import response from "../../../../../assets/response";
 import responseMessage from "../../../../../assets/responseMessage";
+
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'Strict',
+  maxAge: 5 * 60 * 1000,
+};
+
 export class userController {
- 
   async userLogin(req, res, next) {
     try {
-      const { email, password } = req.body;
-      if (email === "admin@gmail.com") {
-        if (password === "admin") {
-          return res.status(200).json(new response({ email, password }, responseMessage.USER_DETAILS));
-        } else {
-          return res.status(401).json(new response({}, responseMessage.INCORRECT_PASSWORD));
-        }
-      } else {
-        return res.status(401).json(new response({}, responseMessage.INCORRECT_EMAIL));
-      }
-    } catch (error) {
-      console.error("Login Error:", error);
-      return res.status(500).json(new response({}, "Internal Server Error"));
-    }
+      return res.status(401).json(new response({}, responseMessage.INCORRECT_EMAIL));
+
+    // } else {
+
+    //   const { email, password } = req.body;
+    //   if (email === "admin@gmail.com") {
+    //     if (password === "admin") {
+    //       res.cookie("authToken", "token_here", cookieOptions);
+    //       return res.status(401).json(new response({}, responseMessage.INCORRECT_PASSWORD));
+    //     }
+    //   } else {
+    //     return res.status(401).json(new response({}, responseMessage.INCORRECT_EMAIL));
+    //   }
+    // } catch (error) {
+    //   console.error("Login Error:", error);
+    //   return res.status(500).json(new response({}, "Internal Server Error"));
+    // }
   }
-  
+
 }
 
 export default new userController();
