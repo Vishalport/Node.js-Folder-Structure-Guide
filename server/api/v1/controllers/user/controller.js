21645c6cc3f72d49f97b883e48449ee6529d0ee8
Vishal Kumar <vishal.kumar@moondive.co>
2024-08-22 16:19:22 +0530

    make the all chnages and ready to Releases v1.24.08.1

--- server/api/v1/controllers/user/controller.js
+++ server/api/v1/controllers/user/controller.js
@@ -13,18 +13,18 @@ const cookieOptions = {
 export class userController {
   async userLogin(req, res, next) {
     try {
-      // return res.status(200).json(new response({}, responseMessage.USER_DETAILS));
-      const { email, password } = req.body;
-      if (email === "admin@gmail.com") {
-        if (password === "admin") {
-          res.cookie("authToken", "token_here", cookieOptions);
-          return res.status(200).json(new response({ email }, responseMessage.USER_DETAILS));
-        } else {
-          return res.status(401).json(new response({}, responseMessage.INCORRECT_PASSWORD));
-        }
-      } else {
-        return res.status(401).json(new response({}, responseMessage.INCORRECT_EMAIL));
-      }
+      return res.status(200).json(new response({}, responseMessage.USER_DETAILS));
+      // const { email, password } = req.body;
+      // if (email === "admin@gmail.com") {
+      //   if (password === "admin") {
+      //     res.cookie("authToken", "token_here", cookieOptions);
+      //     return res.status(200).json(new response({ email }, responseMessage.USER_DETAILS));
+      //   } else {
+      //     return res.status(401).json(new response({}, responseMessage.INCORRECT_PASSWORD));
+      //   }
+      // } else {
+      //   return res.status(401).json(new response({}, responseMessage.INCORRECT_EMAIL));
+      // }
     } catch (error) {
       return res.status(500).json(new response({}, "Internal Server Error"));
     }
